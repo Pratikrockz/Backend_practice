0eee66f0d78d8abac351d4e7c14748305553f65e
Pratikrockz <pratikpahari40@gmail.com>
2023-01-10 19:55:23 +0530

    new file methods added

---
+++ methods.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000);
+
+let users = {};
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  console.log(req.body);
+  users = req.body;
+  res.json({
+    message: "data received successfully",
+    user: req.body,
+  });
+});
+//update the data
+app.patch("/users", (req, res) => {
+  console.log(req.body);
+  let newdata = req.body;
+  for (key in newdata) {
+    users[key] = newdata[key];
+  }
+  res.send(users);
+});
+
+app.delete("/users", (req, res) => {
+  users = {};
+  res.json({
+    message: "delete the value",
+    users: users,
+  });
+});
